d83d4af2246f741ecb7b341c26e27b890aa7ed0a
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-17 16:39:45 -0600

    lib/foo complete. Currently untested.

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -4,6 +4,6 @@ namespace agillikin1\ObjectOriented;
 
 require_once("../Classes/foo.php");
 
-$foo = new author();
+$foo = new author(1,"neo.gif","thebest","author1@hotmail.com","thaehlbshrebgli", "Author1");
 
 var_dump($foo);
