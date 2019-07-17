31a25ccb1fef4bdcd3325f024ccc47a480945d35
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-17 09:37:31 -0600

    Started on getters and setters

--- php/Classes/foo.php
+++ php/Classes/foo.php
@@ -32,6 +32,30 @@ class author (
 	 * The author's username. It is unique.
 	 */
 	private $authorUsername;
+
+	/**
+	 * accessor method for authorId
+	 *
+	 * @return value of authorId (should fit in Binary)
+	 */
+	public function getAuthorId() {
+		return($this->authorId);
+	}
+	/**
+	 * Mutator method for authorId
+	 *
+	 * @param binary $newAuthorId new value of authorId
+	 * @throws UnexpectedValueException if $newAuthorId is not an Int. (Should actually be binary)
+	 */
+	public function setAuthorId(int $newAuthorId) {
+		// verify the author ID is valid
+		$newAuthorId = filter_var($newAuthorId, FILTER_VALIDATE_INT);
+		if ($newAuthorId === false) {
+			throw(new UnexpectedValueException("Author ID is not a valid value"));
+		}
+		$this->authorId = intval($newAuthorId);
+	}
+
 )
 
 >
