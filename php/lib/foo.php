932ecca7b7b3ff4fc7a0b4433c9a80cfc507f1f7
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-18 21:55:38 -0600

    Attempted to place generated Uuid in test constructor. Results inconclusive.

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -4,6 +4,6 @@ namespace agillikin1\ObjectOriented;
 
 require_once("../Classes/foo.php");
 
-$foo = new author(1010101,"neo.gif","thebest","author1@hotmail.com","thaehlbshrebgli", "Author1");
+$foo = new author('9c7d1ba3cfaf42949503cd3a3cc53cd6', "neo.gif","thebest","author1@hotmail.com","thaehlbshrebgli", "Author1");
 
 var_dump($foo);
