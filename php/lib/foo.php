05b6c7d76efa4c5aab084a08390ae3aaa5fa0060
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-22 14:33:54 -0600

    Fixed broken references

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -2,7 +2,7 @@
 
 namespace agillikin1\ObjectOriented;
 
-require_once("../Classes/foo.php");
+require_once("../Classes/author.php");
 
 $foo = new author('9c7d1ba3cfaf42949503cd3a3cc53cd6', "neo.gif","thebest","author1@hotmail.com","thaehlbshrebgli", "Author1");
 
