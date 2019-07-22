6f9ca0d2dd32c5f74f53581533b83be0ac4bb572
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-22 15:40:30 -0600

    Adjusting Uuid

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -4,6 +4,6 @@ namespace agillikin1\ObjectOriented;
 
 require_once("../Classes/author.php");
 
-$foo = new author("9c7d1ba3cfaf42949503cd3a3cc53cd6cc", "neo.gif","thebest","author1@hotmail.com","thaehlbshrebgli", "Author1");
+$foo = new author("6d626593-f558-4670-9075-314835fa752c", "neo.gif","thebest","author1@hotmail.com","thaehlbshrebgli", "Author1");
 
 var_dump($foo);
