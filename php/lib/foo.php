f8809187fca99489c0f4f8bd835d5e79f464ec34
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-24 12:35:15 -0600

    Adjusted test case

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -4,6 +4,6 @@ namespace agillikin1\ObjectOriented;
 
 require_once("../Classes/author.php");
 
-$foo = new author("6d626593-f558-4670-9075-314835fa752c", "neo.gif","thebest","author1@hotmail.com","thaehlbshrebgli", "Author1");
+$foo = new author("6d626593-f558-4670-9075-314835fa752c", "neo.gif","11111111111111thebestisyettocome","author1@hotmail.com","thaehlbshrebgli", "Author1");
 
 var_dump($foo);
